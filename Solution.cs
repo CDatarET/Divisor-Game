91721f154f59fc2b5c17b0445303cb45cec9988f
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-07-21 22:35:01 +0530

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public bool DivisorGame(int n) {
+        if(n % 2 == 0){
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }
+}
